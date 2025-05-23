2ef5ac2ffef49948862a8ce335c45e55e067dfda
SleepySoldier <t.perotti1@gmail.com>
2025-05-23 17:33:25 -0400

    End of section 132

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright SleepySoldier
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,51 @@
+// Copyright SleepySoldier
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Ranger,
+	Warrior
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+	//UPROPERTY to be able to set it from within the asset per class
+	UPROPERTY(EditDefaultsOnly, Category = "ClassDefaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "CharacterClassDefaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+	
+	// To be shared across all Data assets
+	UPROPERTY(EditDefaultsOnly, Category = "CommonClassDefaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+
+	UPROPERTY(EditDefaultsOnly, Category = "CommonClassDefaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	// Function to return the Map info when given A CharacterClass enum
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+};
